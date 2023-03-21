fdbd41a97f60b4ac1fa4b09c7f28cb0467a0026b
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-03-21 15:59:13 -0400

    Alias fails

--- WORKSPACE
+++ WORKSPACE
@@ -10,7 +10,7 @@ load("@rules_rust//rust:repositories.bzl", "rules_rust_dependencies", "rust_regi
 
 rules_rust_dependencies()
 
-RUST_VERSION = "1.67.1"
+RUST_VERSION = "nightly/2023-01-03"
 
 rust_register_toolchains(
     edition = "2021",
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-use rand::{Rng, thread_rng};
-use rand_07::{Rng as Rng2, thread_rng as thread_rng_07};
+use merk_v1::Merk as merk1;
+use merk_v2::Merk as merk2;
 
 fn main() {
-    let rng: u32 = thread_rng().gen();
-    let rng_07: u32 = thread_rng_07().gen();
-
-    println!("rng: {rng}");
-    println!("rng_07: {rng_07}");
 }
